b3603d2e567e72012faa5483212458033fd29fa1
mireille <mimimireille95@gmail.com>
2019-08-30 09:21:13 -0400

    navbar added

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient ,HttpHeaders} from '@angular/common/http';
 
  @Injectable()
-export class ProfileService {
+ 
+  export class ProfileService {
   private username:string;
   private access_token ='f603cfa5afa1d31a966fecacd928a1ab2b9f2b15';
   
@@ -19,7 +20,7 @@ export class ProfileService {
   return this.http.get("https://api.github.com/users/" + this.username + "/repos?access_token=" + 
   this.access_token + "&eccess_token=" +this.access_token);
   }
-updateProfile(username:string){
+  updateProfile(username:string){
   this.username = username;
 }
 
